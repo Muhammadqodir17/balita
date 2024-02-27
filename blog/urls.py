b914fcd4e9e4113cabf25097a5b97860636b3365
Muhammadqodir <km1728.uz@gmail.com>
2024-02-27 14:10:55 +0500

    added

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from .views import home_view, category_view, about_view, contact_view, blog_single_detail_view
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('category/', category_view),
+    path('about/', about_view),
+    path('contact/', contact_view),
+    path('blog/<int:pk>/', blog_single_detail_view)
+]
